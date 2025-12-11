25988a167744b957b1324f16427ca3b48e05115f
David Gili <d.gili.lopez@gmail.com>
2025-12-11 12:41:58 -0300

    pagina registro creada correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import RegisterPage from "./components/RegisterPage";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <RegisterPage></RegisterPage>
       <Footer></Footer>
     </>
   );
